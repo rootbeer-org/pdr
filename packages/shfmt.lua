c4784990e31d6f9912c9efad908a11445e9b5720
Aarnav Tale <git@tale.me>
2026-09-20 10:55:18 -0700

    fix(shfmt): restore source build version stamping

--- packages/shfmt.lua
+++ packages/shfmt.lua
@@ -15,6 +15,9 @@ return {
             url = "https://codeload.github.com/mvdan/sh/tar.gz/refs/tags/{tag}",
             archive = "tar.gz",
             strip_prefix = "sh-{version}",
+            patches = {
+                '--- a/cmd/shfmt/main.go\n+++ b/cmd/shfmt/main.go\n@@ -66,6 +66,8 @@\n }\n \n var (\n+\tversion = "(unknown)"\n+\n \t// Generic flags.\n \tversionFlag = flagVal("", "version", false, flag.BoolVar)\n \tlist        = flagVal("l", "list", "false", boolStringVar)\n@@ -156,8 +158,7 @@\n \tflag.Parse()\n \n \tif versionFlag.val {\n-\t\tversion := "(unknown)"\n-\t\tif info, ok := debug.ReadBuildInfo(); ok {\n+\t\tif info, ok := debug.ReadBuildInfo(); version == "(unknown)" && ok {\n \t\t\tmod := &info.Main\n \t\t\tif mod.Replace != nil {\n \t\t\t\tmod = mod.Replace\n',
+            },
         },
     },
     build = {
@@ -30,7 +33,7 @@ return {
     },
     versions = {
         ["3.14.1"] = {
-            revision = 3,
+            revision = 4,
             inputs = {
                 source = {
                     sha256 = "ec4bdb88ab6c95686be3a4eeb4ad77d2b49d33d2ed7b0a65035cd52d2d87c443",
